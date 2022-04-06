400d7fb3949aa668691b1c1e1cc96db19d082a7b
matthewmccord <matthewmccord3@gmail.com>
2022-04-06 14:29:03 -0600

    trying to figure out a better cookie notification

--- UDOT/Startup.cs
+++ UDOT/Startup.cs
@@ -16,6 +16,7 @@ using Amazon;
 using Amazon.SecretsManager;
 using Amazon.SecretsManager.Model;
 using System.IO;
+using Microsoft.AspNetCore.Http;
 
 namespace UDOT
 {
@@ -110,6 +111,15 @@ namespace UDOT
         {
             services.AddControllersWithViews();
 
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential 
+                // cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                // requires using Microsoft.AspNetCore.Http;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
             services.Configure<IdentityOptions>(options =>
             {
                 // Increased the Required Password Length to 10
@@ -153,6 +163,7 @@ namespace UDOT
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
             app.UseRouting();
 
 
